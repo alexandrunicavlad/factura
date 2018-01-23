4c8923bda51a0b25e74fecf83cc056fbca29b326
Nica Alexandru <alexandrunicavlad@yahoo.com>
2018-01-23 18:25:35 +0200

    add

--- app/src/main/java/com/alexandrunica/factura/AddActivity.java
+++ app/src/main/java/com/alexandrunica/factura/AddActivity.java
@@ -2,24 +2,28 @@ package com.alexandrunica.factura;
 
 import android.app.DatePickerDialog;
 import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 
-public class AddActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener   {
+public class AddActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
 
     private TextView start;
     private TextView end;
+    private FacturaModel facturaModel;
+    private int currentClick;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +31,9 @@ public class AddActivity extends AppCompatActivity implements DatePickerDialog.O
 
         final Calendar myCalendar = Calendar.getInstance();
 
-        EditText furnizor = (EditText) findViewById(R.id.furnizor_edittext);
+        facturaModel = new FacturaModel();
+
+        Spinner furnizor = (Spinner) findViewById(R.id.furnizor_edittext);
         start = (TextView) findViewById(R.id.start_edittext);
         end = (TextView) findViewById(R.id.end_edittext);
         EditText pret = (EditText) findViewById(R.id.pret_edittext);
@@ -36,9 +42,23 @@ public class AddActivity extends AppCompatActivity implements DatePickerDialog.O
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view) {
+                currentClick = start.getId();
                 showHistory();
             }
         });
+
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                currentClick = end.getId();
+                showHistory();
+            }
+        });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        furnizor.setOnItemSelectedListener(this);
+        furnizor.setAdapter(adapter);
     }
 
 
@@ -48,14 +68,30 @@ public class AddActivity extends AppCompatActivity implements DatePickerDialog.O
         setDate(cal);
     }
 
-    private void setDate(final Calendar calendar) {
+    private void setDate (final Calendar calendar) {
         final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        start.setText(dateFormat.format(calendar.getTime()));
+        if (currentClick == R.id.start_edittext) {
+            start.setText(dateFormat.format(calendar.getTime()));
+            facturaModel.setStartDate(calendar.getTime().toString());
+        } else {
+            end.setText(dateFormat.format(calendar.getTime()));
+            facturaModel.setEndDate(calendar.getTime().toString());
+        }
     }
 
     private void showHistory () {
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         DatePickerFragment historyFramgnet = new DatePickerFragment();
         historyFramgnet.show(fragmentTransaction, DatePickerFragment.TAG);
+    }
+
+    @Override
+    public void onItemSelected (AdapterView<?> adapterView, View view, int i, long l) {
+        facturaModel.setFurnizor((String) adapterView.getItemAtPosition(i));
+    }
+
+    @Override
+    public void onNothingSelected (AdapterView<?> adapterView) {
+
     }
 }
